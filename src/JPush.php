c974c0381054e754a410001d459b09371ce0dc57
付广辉 <frowhy@gmail.com>
2017-09-07 12:52:38 +0800

    创建 JPush 单例

--- src/JPush.php
+++ src/JPush.php
@@ -8,10 +8,27 @@
 
 namespace Frowhy\JPush;
 
+
 class JPush
 {
+    private $app_key;
+    private $master_secret;
+    private $JPush;
+
     public function __construct(string $app_key, string $master_secret)
     {
-        return new \JPush($app_key, $master_secret);
+        $this->app_key       = $app_key;
+        $this->master_secret = $master_secret;
+    }
+
+    public function getInstance()
+    {
+        if ($this->JPush === null) {
+            $this->JPush = new \JPush($this->app_key, $this->master_secret);
+        } else {
+            $this->JPush;
+        }
+
+        return $this->JPush;
     }
 }
